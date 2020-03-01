2d6c68d8f026eade695f09d55201ced9995faeb7
Randy <randyisbusy@gmail.com>
2020-03-01 13:30:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(arg1, arg2){
+      let x =  arg1.filter( val => val.toLowerCase() === arg2.toLowerCase() )
+        return x
+}
+
+function fuzzyMatch(arg1, arg2){   
+    let regEx = `^${arg2}`; 
+    
+       let x = arg1.filter( val => val.match( regEx , val ) ); 
+       console.log(x)
+    return x
+} 
+
+function matchName(arg1, arg2){
+    let response = arg1.filter(element => element.name === arg2) 
+    return response    
+    
+}
